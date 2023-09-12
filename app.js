0b1a672e9d60dc5a4fbd4d7a99442636c091942f
Vasiliy <vasiliyalexsandrovich@gmail.com>
2023-09-12 21:50:26 +0500

    js1

---
+++ app.js
@@ -0,0 +1,28 @@
+
+var imya = prompt('Ваше имя')
+console.log(imya);
+var age = +prompt('Ваш возраст')
+console.log(age);
+var primer1 = prompt('Решите пример 1: 6 + 3 = '  )
+console.log('Пример 1: 6 + 3 = 9. Ваш ответ : ' + primer1 );
+var primer2 = prompt('Решите пример 2: 9 - 2 = '  )
+console.log('Пример 2: 9 - 2 = 7. Ваш ответ : ' + primer2 );
+var primer3 = prompt('Решите пример 3: 8 / 2 = '  )
+console.log('Пример 3: 8 / 2 = 4. Ваш ответ : ' + primer3 );
+var primer4 = prompt('Решите пример 4: 4 * 3 = '  )
+console.log('Пример 4: 4 * 3 = 12. Ваш ответ : ' + primer4 );
+var primer5 = prompt('Решите пример 5: 14 % 3 = '  )
+console.log('Пример 5: 14 % 3 = 2. Ваш ответ : ' + primer5 )
+alert( 'Зайдите в консоль')
+
+
+
+var number1 = +prompt('число X')
+console.log(number1);
+var number2 = +prompt('число Y')
+console.log(number2);
+var number3 = +prompt('число Z')
+console.log(number3);
+var res = ((number1+number2+number3)/3);
+console.log(res);
+alert('среднее арифмитическое число :' + res )
